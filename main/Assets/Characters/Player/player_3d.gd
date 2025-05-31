95373a61fc2885e0b76bddd68320c5a0e36dcddc
Burn't <burntwyrm@gmail.com>
2025-05-31 15:45:04 +1200

    sandbox, player and assets
    NEW
    +Sandbox Map
    +Player
    +- Added and scripted player state and statemachine
    +- State Nodes (no code yet)
    
    ASSETS
    + kenney_prototype-textures
    + box morph
    
    BLENDER
    + Completed basic morph assets

---
+++ main/Assets/Characters/Player/player_3d.gd
@@ -0,0 +1,18 @@
+class_name Player
+extends RigidBody3D
+
+@onready var player_state_machine: Node = $PlayerStateMachine
+
+func _ready() -> void:
+	# Initialize the state machine, passing a reference of the player to the different states
+	# That way they can react and function accordingly
+	player_state_machine.init(self)
+
+func _unhandled_input(event: InputEvent) -> void:
+	player_state_machine.process_input(event)
+
+func _physics_process(delta: float) -> void:
+	player_state_machine.process_physics(delta)
+
+func _process(delta: float) -> void:
+	player_state_machine.process_frame(delta)
